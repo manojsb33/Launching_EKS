946ed879352e6cad4c200f32873bf499a89e4a73
Manoj S B <131129578+manojsb33@users.noreply.github.com>
2024-03-17 00:57:40 +0530

    Create backend.tf

---
+++ Eks-terraform/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "my-first-achivement-in" # Replace with your actual S3 bucket name
+    key    = "Jenkins/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
