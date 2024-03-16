c10aace44f088c5c72bed69c5589aabcd117ba18
Manoj S B <131129578+manojsb33@users.noreply.github.com>
2024-03-17 01:28:16 +0530

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-game"
+  name               = "eks-cluster-gamee"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -49,7 +49,7 @@ resource "aws_eks_cluster" "example" {
 }
 
 resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
+  name = "eks-node-group-game"
 
   assume_role_policy = jsonencode({
     Statement = [{
